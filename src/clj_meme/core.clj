eb22dd3649c4faafa3942cebf1f8b7c0eb72fbcb
Jared Lobberecht <pretender314@gmail.com>
2013-12-27 23:53:15 -0600

    removing memeworker java class

--- src/clj_meme/core.clj
+++ src/clj_meme/core.clj
@@ -1,10 +1,21 @@
 (ns clj-meme.core
   (:require [clojure.java.io :as io])
-  (:import [com.amazonaws.memes MemeWorker]))
+  (:import [com.amazonaws.memes ImageOverlay]
+           [javax.imageio ImageIO]
+           [java.io ByteArrayOutputStream]))
+
+(defn readImageIntoBuffer [image]
+  (let [baos (ByteArrayOutputStream.)]
+    (try
+      (ImageIO/write image "png" baos)
+      (.toByteArray baos)
+      (finally (.close baos)))))
 
 ;; Returns PNG compressed byte[] data
 (defn render-image [image-path top-caption bottom-caption]
-  (MemeWorker/processImage (io/file image-path) top-caption bottom-caption))
+  (let [base (ImageIO/read (io/file image-path))
+        rendered (ImageOverlay/overlay base top-caption bottom-caption)]
+    (readImageIntoBuffer rendered)))
 
 (defn generate-image! [image-path top-caption bottom-caption output-path]
   (let [content (render-image image-path top-caption bottom-caption)]
